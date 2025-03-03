7cb1543bac07b731416e4bfc0b56c0b69687cfd9
Fédi <123569039+Fedi78@users.noreply.github.com>
2025-03-03 14:58:58 +0100

    bdd update info

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: 'edifis_pro',
     port: process.env.DB_PORT,
 }).promise();
 
