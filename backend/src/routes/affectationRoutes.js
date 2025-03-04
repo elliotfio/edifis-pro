e691f4ac922924e9ed685f5b9b7754c239144322
karim <kar.rakem@gmail.com>
2025-03-04 17:41:10 +0100

    affectationRoutes

--- backend/src/routes/affectationRoutes.js
+++ backend/src/routes/affectationRoutes.js
@@ -1,1 +1,13 @@
 const pool = require('../config/db');
+
+//recup les affection 
+const getAffectationsByChantier = async (req, res) => {
+    const chantier_id = req.params.chantier_id;
+
+    try {
+        const [rows] = await pool.query('SELECT * FROM affectations WHERE chantier_id = ?', [chantier_id]);
+        res.json(rows);
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur serveur', error });
+    }
+};
